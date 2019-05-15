d3a17397a20abd40c4f5f6933eea25cd7623338b
Asher Dawes <asher.dawes@gmail.com>
2019-05-15 16:45:07 -0700

    troubleshooting dylib linking

--- makeReleaseMac.sh
+++ makeReleaseMac.sh
@@ -12,7 +12,6 @@ mv verus-cli/fetch-params.sh verus-cli/fetch-params
 chmod +x ${KMD_DIR}/fetch-params
 chmod +x ${KMD_DIR}/verus
 chmod +x ${KMD_DIR}/verusd
-chmod +x ${KMD_DIR}/upgrade-agama.sh
 
 binaries=("komodo-cli" "komodod")
 alllibs=()
@@ -22,12 +21,13 @@ do
     cp src/${binary} ${KMD_DIR}
     # find the dylibs to copy for komodod
     DYLIBS=`otool -L ${KMD_DIR}/${binary} | grep "/usr/local" | awk -F' ' '{ print $1 }'`
+    DYLIBS+=" /usr/local/opt/libidn2/lib/libidn2.0.dylib"
     echo "copying ${DYLIBS} to ${KMD_DIR}"
     # copy the dylibs to the srcdir
     for dylib in ${DYLIBS}; do cp -rf ${dylib} ${KMD_DIR}; done
 done
 
-libraries=("libgcc_s.1.dylib" "libgomp.1.dylib" "libidn2.4.dylib" "libstdc++.6.dylib")
+libraries=("libgcc_s.1.dylib" "libgomp.1.dylib" "libidn2.0.dylib" "libstdc++.6.dylib")
 
 for binary in "${libraries[@]}";
 do
@@ -43,7 +43,7 @@ indirectlibraries=("libintl.8.dylib" "libunistring.2.dylib")
 for binary in "${indirectlibraries[@]}";
 do
     # Need to undo this for the dylibs when we are done
-    chmod 755 src/${binary}
+    chmod 755 ${KMD_DIR}/${binary}
     # find the dylibs to copy for komodod
     DYLIBS=`otool -L ${KMD_DIR}/${binary} | grep "/usr/local" | awk -F' ' '{ print $1 }'`
     echo "copying indirect ${DYLIBS} to ${KMD_DIR}"
